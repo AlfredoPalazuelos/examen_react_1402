f5124c3c81e3cb0fa74155d50fe862aed166ece8
AlfredoPalazuelos <2002alfredo@gmail.com>
2022-02-14 14:01:20 +0100

    ultimo retoques

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './menu.css'
 
 import {
   Container,
@@ -27,7 +28,7 @@ class Menu extends React.Component {
         <Container fluid>
           <Navbar.Brand>Menu  <img
             className="logo-image"
-            src='src/assets/images/logo.png' alt='' className='logo'
+            src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png' alt='' className='logo'
           /></Navbar.Brand>
           <Navbar.Toggle aria-controls="offcanvasNavbar" />
           <Navbar.Offcanvas
